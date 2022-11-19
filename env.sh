65394c4eff1c1cd97bd2239adc4f20bb7a114888
halsallaaron95 <104553148+halsallaaron95@users.noreply.github.com>
2022-11-19 17:06:20 +0800

    update env.sh

--- env.sh
+++ env.sh
@@ -1,17 +1,3 @@
-##mount the disk if needed
-#mkdir -p /volume_16756
-#ls /dev/disk/by-id/
-#mkfs.ext4 -F /dev/disk/by-id/scsi-0BUYVM_SLAB_VOLUME-16756
-#mount -o discard,defaults /dev/disk/by-id/scsi-0BUYVM_SLAB_VOLUME-16756 /volume_16756
-#echo "/dev/disk/by-id/scsi-0BUYVM_SLAB_VOLUME-16756 /volume_16756 ext4 defaults 0 0" >>/etc/fstab
-#chmod -R 777 /volume_16756/
-
-##create user if needed
-#sudo useradd hitomi_fast
-#sudo passwd hitomi_fast
-#sudo usermod -aG sudo hitomi_fast
-#sudo mkhomedir_helper hitomi_fast
-
 #install chrome for selenium
 sudo apt update
 sudo apt-get install chromium-chromedriver
